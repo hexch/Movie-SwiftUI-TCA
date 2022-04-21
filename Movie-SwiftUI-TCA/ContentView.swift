759a352545934aad8b9611244b3b0ebfec29a85d
hexch <hexch.c@gmail.com>
2022-04-21 12:50:00 +0900

    Initial Commit

---
+++ Movie-SwiftUI-TCA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie-SwiftUI-TCA
+//
+//  Created by XIAOCHUAN HE on R 4/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movie-SwiftUI-TCA/Movie_SwiftUI_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_SwiftUI_TCAApp.swift
+//  Movie-SwiftUI-TCA
+//
+//  Created by XIAOCHUAN HE on R 4/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Movie_SwiftUI_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
